e071c8fe563316194730eff84613efb60e13383e
Your Name <you@example.com>
2021-03-27 17:30:05 +0200

    More Brick stuff.

--- Assets/Veetu_Brick.cs
+++ Assets/Veetu_Brick.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Veetu_Brick : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+ 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.CompareTag ("Ball"))
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
