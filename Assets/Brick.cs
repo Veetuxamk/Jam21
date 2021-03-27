ee8f12a3ba3445c2059b95acaafa87d606c85cc1
Your Name <you@example.com>
2021-03-27 19:07:41 +0200

    Git safe

--- Assets/Brick.cs
+++ Assets/Brick.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
-  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
-       
+        
     }
 }
