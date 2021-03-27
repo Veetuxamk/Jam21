6faa4d937ca180995cead310f43568876b30df72
trihai264 <53052320+trihai264@users.noreply.github.com>
2021-03-27 15:43:59 +0200

    start the ball
    lock ball into platform and launch the ball

---
+++ Noobs/Assets/Hai_Script/Ball.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // config param 
+    [SerializeField] Platform_Script platform1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+
+    // state
+    Vector2 platformToBallVector;
+
+    bool hasStarted = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platformToBallVector = transform.position - platform1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPlatform();
+            LaunchBall();
+        }
+        
+    }
+
+    private void LaunchBall()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+
+    }
+
+    private void LockBallToPlatform()
+    {
+        Vector2 platformPos = new Vector2(platform1.transform.position.x, platform1.transform.position.y);
+        transform.position = platformPos + platformToBallVector;
+    }
+}
