c551a8b57f30a7bba94b6aff59f3a52d5f5c6df1
Your Name <you@example.com>
2021-03-27 09:24:00 +0200

    Platform movement and edge stop

---
+++ Noobs/Assets/Platform_Script.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform_Script : MonoBehaviour
+{
+    public float speed;
+    public float RScreenEdge;
+    public float LScreenEdge;
+    private void Update()
+    {
+        //Gets the inputs from Unitys Input manager
+        float horizontal = Input.GetAxis("Horizontal");
+
+        //Moves the platform with unitys inputs
+        transform.Translate(Vector2.right * horizontal * Time.deltaTime * speed);
+
+        //These stop movement if we hit the screen edge. Our screens edges x position determines L and R ScreenEdges.
+        if(transform.position.x < LScreenEdge)
+        {
+            transform.position = new Vector2(LScreenEdge, transform.position.y);
+        }
+        if (transform.position.x > RScreenEdge)
+        {
+            transform.position = new Vector2(RScreenEdge, transform.position.y);
+        }
+    }
+}
