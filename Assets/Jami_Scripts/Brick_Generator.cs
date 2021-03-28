bbc740488ce1c45c9752bd5ad59b75babc6bf9a6
jami989 <71631114+jami989@users.noreply.github.com>
2021-03-28 12:58:32 +0300

    added music

--- Assets/Jami_Scripts/Brick_Generator.cs
+++ Assets/Jami_Scripts/Brick_Generator.cs
@@ -58,28 +58,28 @@ public class Brick_Generator : MonoBehaviour
                         case 0: case 1:/**/ case 2: /*case 6:*/
                             break;
                         case 4: case 5: case 9:
-                            Instantiate(Basic_1, new Vector2(x, y + 6), Quaternion.identity);
-                            Instantiate(Basic_1, new Vector2(-x, y + 6), Quaternion.identity);
+                            Instantiate(Basic_1, new Vector2(x, y + 5), Quaternion.identity);
+                            Instantiate(Basic_1, new Vector2(-x, y + 5), Quaternion.identity);
                             break;
                         case 3:/**/ case 7: case 8:/* case 10: case 12:*/
-                            Instantiate(Basic_2, new Vector2(x, y + 6), Quaternion.identity);
-                            Instantiate(Basic_2, new Vector2(-x, y + 6), Quaternion.identity);
+                            Instantiate(Basic_2, new Vector2(x, y + 5), Quaternion.identity);
+                            Instantiate(Basic_2, new Vector2(-x, y + 5), Quaternion.identity);
                             break;
                         case 10: case 11: case 12: case 13:/*case 11: case 13: case 15: case 18: case 19: case 20:*/
-                            Instantiate(Basic_3, new Vector2(x, y + 6), Quaternion.identity);
-                            Instantiate(Basic_3, new Vector2(-x, y + 6), Quaternion.identity);
+                            Instantiate(Basic_3, new Vector2(x, y + 5), Quaternion.identity);
+                            Instantiate(Basic_3, new Vector2(-x, y + 5), Quaternion.identity);
                             break;
                         /*case 4:
-                            Instantiate(Repulse_1, new Vector2(x, y + 6), Quaternion.identity);
-                            Instantiate(Repulse_1, new Vector2(-x, y + 6), Quaternion.identity);
+                            Instantiate(Repulse_1, new Vector2(x, y + 5), Quaternion.identity);
+                            Instantiate(Repulse_1, new Vector2(-x, y + 5), Quaternion.identity);
                             break;
                         case 5:
-                            Instantiate(Laser_1, new Vector2(x, y + 6), Quaternion.identity);
-                            Instantiate(Laser_1, new Vector2(-x, y + 6), Quaternion.identity);
+                            Instantiate(Laser_1, new Vector2(x, y + 5), Quaternion.identity);
+                            Instantiate(Laser_1, new Vector2(-x, y + 5), Quaternion.identity);
                             break;*/
                         case 14: case 15: case 16:/*case 6:*/
-                            Instantiate(Durable_1, new Vector2(x, y + 6), Quaternion.identity);
-                            Instantiate(Durable_1, new Vector2(-x, y + 6), Quaternion.identity);
+                            Instantiate(Durable_1, new Vector2(x, y + 5), Quaternion.identity);
+                            Instantiate(Durable_1, new Vector2(-x, y + 5), Quaternion.identity);
                             break;
                     }
 
@@ -95,22 +95,22 @@ public class Brick_Generator : MonoBehaviour
                             case 4: case 5:/**/case 9: /*case 6:*/
                                 break;
                             case 0: case 1: case 2:
-                                Instantiate(Basic_1, new Vector2(0, y + 6), Quaternion.identity);
+                                Instantiate(Basic_1, new Vector2(0, y + 5), Quaternion.identity);
                                 break;
                             case 3:/**/ case 7: case 8:/* case 10: case 12:*/
-                                Instantiate(Basic_2, new Vector2(0, y + 6), Quaternion.identity);
+                                Instantiate(Basic_2, new Vector2(0, y + 5), Quaternion.identity);
                                 break;
                             case 10: case 11: case 12: case 13:/*case 11: case 13: case 15: case 18: case 19: case 20:*/
-                                Instantiate(Basic_3, new Vector2(0, y + 6), Quaternion.identity);
+                                Instantiate(Basic_3, new Vector2(0, y + 5), Quaternion.identity);
                                 break;
                             /*case 7: case 14:
-                                Instantiate(Repulse_1, new Vector2(0, y + 6), Quaternion.identity);
+                                Instantiate(Repulse_1, new Vector2(0, y + 5), Quaternion.identity);
                                 break;
                             case 8: case 16:
-                                Instantiate(Laser_1, new Vector2(0, y + 6), Quaternion.identity);
+                                Instantiate(Laser_1, new Vector2(0, y + 5), Quaternion.identity);
                                 break;*/
                             case 14: case 15: case 16: /*case 9: case 17:*/
-                                Instantiate(Durable_1, new Vector2(0, y + 6), Quaternion.identity);
+                                Instantiate(Durable_1, new Vector2(0, y + 5), Quaternion.identity);
                                 break;
                         }
                     }
