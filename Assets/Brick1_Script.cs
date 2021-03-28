a8438e4707f29a6d4fe9fea3420a701a03f5f0ab
Your Name <you@example.com>
2021-03-28 10:35:39 +0300

    Sound commit

--- Assets/Brick1_Script.cs
+++ Assets/Brick1_Script.cs
@@ -11,17 +11,18 @@ public class Brick1_Script : MonoBehaviour
     void Start()
     {
         timer = Time.realtimeSinceStartup;
+        AudioSource.PlayClipAtPoint(Splosion, Camera.main.transform.position);
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    void OnCollisionEnter2D(Collision2D other)
     {
-        AudioSource.PlayClipAtPoint(Splosion, gameObject.transform.position);
+        
 
-        if (timer > 0)
+        if (timer > 1)
         {
             if (other.transform.CompareTag("Ball"))
             {
-                
+                GetComponent<AudioSource>().Play();
                 Destroy(this.gameObject);
             }
         }
--- Assets/Veetu_Brick.cs
+++ Assets/Veetu_Brick.cs
@@ -8,17 +8,20 @@ public class Veetu_Brick : MonoBehaviour
     public GameObject Brickaruu;
     private float timer;
 
+    public AudioClip Splosion;
     private void Start()
     {
         timer = Time.realtimeSinceStartup;
+        AudioSource.PlayClipAtPoint(Splosion, Camera.main.transform.position);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (timer > 0)
+        if (timer > 1)
         {
              if (other.transform.CompareTag ("Ball"))
              {
+                GetComponent<AudioSource>().Play();
                 Object.Instantiate(Brickaruu, transform.position, Quaternion.identity);
                 Destroy(this.gameObject);
              }
